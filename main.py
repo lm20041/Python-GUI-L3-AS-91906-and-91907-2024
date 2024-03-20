2b768eb02f64e406fad4ba0dea72903054ee58d7
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-03-20 03:46:21 +0000

    03 converter GUI part1 (heading)

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from tkinter import *
 
 class Converter:
-  print("hello world")
+  def __init__(self):
+    # set up GUI Frame
+    self.temp_frame = Frame(padx=10, pady=10)
+    self.temp_frame.grid()
+    self.temp_heading = Label(self.temp_frame, text="Temperature Converter", font=("Arial", "16", "bold"))
+    self.temp_heading.grid(row=0)
 
 #main routine
 if __name__ == "__main__":
