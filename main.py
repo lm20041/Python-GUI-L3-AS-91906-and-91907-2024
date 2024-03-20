307072d99eb50c8e74623612f764b79ca8ef56ab
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-03-20 03:53:16 +0000

    03 converter gui part1(WrapText)
    instructions = "please enter a temperature in either degrees C or F"
        self.temp_instructions = Label(self.temp_frame, text=instructions, wrap=250, width=40, justify="left")
        self.temp_instructions.grid(row=1)

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ class Converter:
     self.temp_frame.grid()
     self.temp_heading = Label(self.temp_frame, text="Temperature Converter", font=("Arial", "16", "bold"))
     self.temp_heading.grid(row=0)
+    
+    instructions = "please enter a temperature in either degrees C or F"
+    self.temp_instructions = Label(self.temp_frame, text=instructions, wrap=250, width=40, justify="left")
+    self.temp_instructions.grid(row=1)
 
 #main routine
 if __name__ == "__main__":
