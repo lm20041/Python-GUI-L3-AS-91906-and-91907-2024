a12110247447cc19caa8fd0b28d2888881b4ae85
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-03-20 03:38:47 +0000

    03 converter GUI part1

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+class Converter:
+  print("hello world")
+
+#main routine
+if __name__ == "__main__":
+  root = Tk()
+  root.title("Temperature Converter")
+  Converter()
+  root.mainloop()
