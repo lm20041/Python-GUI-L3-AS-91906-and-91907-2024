8b6f2b1a58c82b57c16816bceaa60819d6523168
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-03-21 01:05:11 +0000

    04 converter gui part2 (button add on)
    button_font = ("Arial", "12", "bold")
        button_fg = "#FFFFFF"
    #====================#
    self.to_celsius_button = Button(self.button_frame, text="To Celsius", bg="#990099", fg=button_fg, font=button_font)
        self.to_celsius_button.grid(row=0, column=0)
        self.to_farenheit_button = Button(self.button_frame, text="To Farenheit", bg="#009900", fg=button_fg, font=button_font)
        self.to_farenheit_button.grid(row=0, column=1)

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from tkinter import *
 
 class Converter:
   def __init__(self):
+    #common format all buttons
+    # Arial size 14 bold with white text
+    button_font = ("Arial", "12", "bold")
+    button_fg = "#FFFFFF"
     # set up GUI Frame
     self.temp_frame = Frame(padx=10, pady=10)
     self.temp_frame.grid()
@@ -20,8 +24,11 @@ class Converter:
     # convert button
     self.button_frame = Frame(self.temp_frame)
     self.button_frame.grid(row=4)
-    self.to_celsius_button = Button(self.button_frame, text="To Degrees C", bg="#990099", fg="#FFFFFF")
+    
+    self.to_celsius_button = Button(self.button_frame, text="To Celsius", bg="#990099", fg=button_fg, font=button_font)
     self.to_celsius_button.grid(row=0, column=0)
+    self.to_farenheit_button = Button(self.button_frame, text="To Farenheit", bg="#009900", fg=button_fg, font=button_font)
+    self.to_farenheit_button.grid(row=0, column=1)
 
 #main routine
 if __name__ == "__main__":
