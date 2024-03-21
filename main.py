a5ecae757bd40bc936b93134e3df53be2dcd24cf
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-03-21 00:14:28 +0000

    04 converter gui part2 (entry, Error)

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ class Converter:
     instructions = "please enter a temperature in either degrees C or F"
     self.temp_instructions = Label(self.temp_frame, text=instructions, wrap=250, width=40, justify="left")
     self.temp_instructions.grid(row=1)
+    self.temp_entry = Entry(self.temp_frame, font=("Arial", "14"))
+    self.temp_entry.grid(row=2, padx=10, pady=10)
+    error = "*Please enter a number"
+    self.temp_error = Label(self.temp_frame, text=error, fg="red")
+    self.temp_error.grid(row=3)
 
 #main routine
 if __name__ == "__main__":
