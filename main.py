1166c7a928a28c3f91b7b12063cf03c2ca4a8791
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-03-22 00:20:59 +0000

    11 History GUI(screen)

--- main.py
+++ main.py
@@ -22,7 +22,18 @@ class Converter:
     DisplayHelp()
 class DisplayHelp:
   def __init__(self):
+    background = "#ffe6cc"
+    self.help_box = Toplevel()
+    self.help_frame = Frame(self.help_box, width=300, height=200, bg=background)
     print("you pressed help")
+    self.help_frame.grid()
+    self.help_heading_label = Label(self.help_frame, bg=background, text="Help / Info", font=("Arial", "14", "bold"))
+    self.help_heading_label.grid(row=0)
+    help_text = "to use this program, enter a temperature in either degrees C or F and click one of the buttons to convert it to the other... \n \n Note that the program will not work if you enter a temperature that is less than -273.15 degrees Celsius or -459.67 degrees Fahrenheit... \n \n You can also click the 'History / Export' button to see a list of your conversions..."
+    self.help_text_label = Label(self.help_frame, bg=background, text=help_text, wrap=350, justify="left")
+    self.help_text_label.grid(row=1, padx=10)
+    self.dismiss_button = Button(self.help_frame, font=("Arial", "12", "bold"), text="Dismiss", bg="#CC6600", fg="#FFFFFF")
+    self.dismiss_button.grid(row=2, padx=10, pady=10)
   
 #main routine
 if __name__ == "__main__":
