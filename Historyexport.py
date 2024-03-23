8f4c56e41701f5ec6a1b58e3ff0a199c16799d7f
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-03-23 21:14:36 +0000

    13 History GUI part 1 (detail screen)
    i was able to record the screen detail in rows of code.
    but i have not yet figure out how to get the cal list history into it.

--- Historyexport.py
+++ Historyexport.py
@@ -5,43 +5,76 @@ class Converter:
   def __init__(self):
     #common format all buttons
     # Arial size 14 bold with white text
-    button_font = ("Arial", "12", "bold")
+    button_font_12 = ("Arial", "12", "bold")
+    button_font_14 = ("Arial", "14", "bold")
+    button_font_16 = ("Arial", "16", "bold")
     button_fg = "#FFFFFF"
+
+    # Five E.G
+    # self.all_calculations = ['0 F = -17.78 C', '0 C = -17.78 F', '100 C = 212 F', '40 F = 104 C', '-40 C = -40 F', '-40 F = -40 C', '-273.15 C = -273.15 F']
+    # six item list
+    #self.all_calculations = ['0 F = -18 C', '0 C = 32 F', '30 F is -1 C', '30 C is 86 F', '100 C is 212 F', '40 F is 4 C']
+
+
     # set up GUI Frame
     self.temp_frame = Frame(padx=10, pady=10)
     self.temp_frame.grid()
 
     # convert button
-    self.button_frame = Frame(self.temp_frame)
+    self.button_frame = Frame(padx=30, pady=30)
     self.button_frame.grid(row=0)
 
     # sitch screens button
-    self.to_history_button = Button(self.button_frame, text="Help / Info", bg="#CC6600", fg=button_fg, font=button_font, width=12, command=self.to_history)
-    self.to_history_button.grid(row=1, column=0, padx=5, pady=5)
+    self.to_history_button = Button(self.button_frame, text="History / Export", bg="#004C99", fg=button_fg, font=button_font_12, width=12, state=DISABLED, command=self.to_history)
+    self.to_history_button.grid(row=1, column=1, padx=5, pady=5)
+    # **** Remove when integrating!! ****
+    self.to_history_button.config(state=NORMAL)
 
   def to_history(self):
     DisplayHistory(self)
 
 class DisplayHistory:
   def __init__(self, partner):
-    #set up dialogue box and background colour
-    background = "#ffe6cc"
     self.history_box = Toplevel()
     # disable help button
     partner.to_history_button.config(state=DISABLED)
     # If users press cross at top, closes help and 'releases' help button
     self.history_box.protocol('WM_DELETE_WINDOW', partial(self.close_history, partner))
 
-    self.history_frame = Frame(self.history_box, width=300, height=200, bg=background)
-    print("you pressed help")
+    self.history_frame = Frame(self.history_box, width=300, height=200)
     self.history_frame.grid()
-    self.history_heading_label = Label(self.history_frame, bg=background, text="Help / Info", font=("Arial", "14", "bold"))
+
+    self.history_heading_label = Label(self.history_frame, text="History / Export", font=button_font_14)
     self.history_heading_label.grid(row=0)
-    help_text = "to use this program, enter a temperature in either degrees C or F and click one of the buttons to convert it to the other... \n \n Note that the program will not work if you enter a temperature that is less than -273.15 degrees Celsius or -459.67 degrees Fahrenheit... \n \n You can also click the 'History / Export' button to see a list of your conversions..."
-    self.history_button_text_label = Label(self.history_frame, bg=background, text=help_text, wrap=350, justify="left")
-    self.history_button_text_label.grid(row=1, padx=10)
-    self.dismiss_button = Button(self.history_frame, font=("Arial", "12", "bold"), text="Dismiss", bg="#CC6600", fg="#FFFFFF", command=partial(self.close_history, partner))
-    self.dismiss_button.grid(row=2, padx=10, pady=10)
+
+    # History text and label
+    history_text = "Below are your recent CAL - \n showing 3 / 3 CAL. \n ALL CALS are shown to the nearest degree. \n"
+    self.text_instructions_label = Label(self.history_frame, text=history_text, width=45, justify="left", wraplength=300, padx=10, pady=10)
+    self.text_instructions_label.grid(row=1, padx=10)
+
+    self.all_calcs_label = Label(self.history_frame, bg=background, text="CAL go here", padx=10, pady=10, bg="#ffe6cc", width=40, justify="left")
+    self.all_calcs_label.grid(row=2)
+
+    # instuctions for saving files
+    save_text = "Either choose a custom file name (and push \n <Export>) or simply push <Export> to save your \n CAL to a text file. if the \n file already exists, it will be overwritten."
+    self.save_instructions_label = Label(self.history_frame, text=save_text, wraplength=300, justify="left", width=40, padx=10, pady=10)
+    self.save_instructions_label.grid(row=3)
+
+    #filename entry widget, white background to start
+    self.filename_entry = Entry(self.history_frame, font=("Arial", "12"), bg="#ffffff", width=25)
+    self.filename_entry.grid(row=4)
+
+    self.filename_error_label = Label(self.history_frame, text="Filename error goes here", fg="#9C0000", font=button_font_12)
+    self.filename_error_label.grid(row=5)
+
+    self.button_frame = Frame(self.history_frame)
+    self.button_frame.grid(row=6)
+
+    self.export_button = Button(self.button_frame, font=button_font_12, text="Export", bg="#004C99", fg="#FFFFFF", width=12)
+    self.export_button.grid(row=0, column=0, padx=10, pady=10)
+
+    self.dismiss_button = Button(self.history_frame, font=button_font_12, text="Dismiss", bg="#666666", fg="#FFFFFF", width=12, command=partial(self.close_history, partner))
+    self.dismiss_button.grid(row=0, column=1, padx=10, pady=10)
 
   # closes help dialogue (used by button and x at top of dialogue)
   def close_history(self, partner):
--- main.py
+++ main.py
@@ -1,57 +1,76 @@
 from tkinter import *
-from functools import partial # to prevent unwanted windows
+from functools import partial
 
 class Converter:
-  def __init__(self):
-    #common format all buttons
-    # Arial size 14 bold with white text
-    button_font = ("Arial", "12", "bold")
-    button_fg = "#FFFFFF"
-    # set up GUI Frame
-    self.temp_frame = Frame(padx=10, pady=10)
-    self.temp_frame.grid()
+    def __init__(self):
+        button_font_12 = ("Arial", "12", "bold")
+        button_font_14 = ("Arial", "14", "bold")
+        button_fg = "#FFFFFF"
 
-    # convert button
-    self.button_frame = Frame(self.temp_frame)
-    self.button_frame.grid(row=0)
+        self.temp_frame = Frame(padx=10, pady=10)
+        self.temp_frame.grid()
 
-    # sitch screens button
-    self.to_history_button = Button(self.button_frame, text="Help / Info", bg="#CC6600", fg=button_fg, font=button_font, width=12, command=self.to_history)
-    self.to_history_button.grid(row=1, column=0, padx=5, pady=5)
+        self.button_frame = Frame(padx=30, pady=30)
+        self.button_frame.grid(row=0)
 
-  def to_history(self):
-    DisplayHistory(self)
+        self.to_history_button = Button(self.button_frame, text="History / Export", bg="#004C99", fg=button_fg, font=button_font_12, width=12, state=DISABLED, command=self.to_history)
+        self.to_history_button.grid(row=1, column=1, padx=5, pady=5)
+
+    def to_history(self):
+        DisplayHistory(self)
 
 class DisplayHistory:
-  def __init__(self, partner):
-    #set up dialogue box and background colour
-    background = "#ffe6cc"
-    self.history_box = Toplevel()
-    # disable help button
-    partner.to_history_button.config(state=DISABLED)
-    # If users press cross at top, closes help and 'releases' help button
-    self.history_box.protocol('WM_DELETE_WINDOW', partial(self.close_history, partner))
+    def __init__(self, partner):
+        self.background = "#ffe6cc"
+        button_font_12 = ("Arial", "12", "bold")
+        self.history_box = Toplevel()
+        partner.to_history_button.config(state=DISABLED)
+        self.history_box.protocol('WM_DELETE_WINDOW', partial(self.close_history, partner))
 
-    self.history_frame = Frame(self.history_box, width=300, height=200, bg=background)
-    print("you pressed help")
-    self.history_frame.grid()
-    self.history_heading_label = Label(self.history_frame, bg=background, text="Help / Info", font=("Arial", "14", "bold"))
-    self.history_heading_label.grid(row=0)
-    help_text = "to use this program, enter a temperature in either degrees C or F and click one of the buttons to convert it to the other... \n \n Note that the program will not work if you enter a temperature that is less than -273.15 degrees Celsius or -459.67 degrees Fahrenheit... \n \n You can also click the 'History / Export' button to see a list of your conversions..."
-    self.history_button_text_label = Label(self.history_frame, bg=background, text=help_text, wrap=350, justify="left")
-    self.history_button_text_label.grid(row=1, padx=10)
-    self.dismiss_button = Button(self.history_frame, font=("Arial", "12", "bold"), text="Dismiss", bg="#CC6600", fg="#FFFFFF", command=partial(self.close_history, partner))
-    self.dismiss_button.grid(row=2, padx=10, pady=10)
+        self.history_frame = Frame(self.history_box, width=300, height=200)
+        self.history_frame.grid()
 
-  # closes help dialogue (used by button and x at top of dialogue)
-  def close_history(self, partner):
-      #put help button back to normal...
-      partner.to_history_button.config(state=NORMAL)
-      self.history_box.destroy()
+        self.history_heading_label = Label(self.history_frame, text="History / Export", font=button_font_14)
+        self.history_heading_label.grid(row=0)
 
-#main routine
+        history_text = "Below are your recent CAL - \n showing 3 / 3 CAL. \n ALL CALS are shown to the nearest degree. \n"
+        self.text_instructions_label = Label(self.history_frame, text=history_text, width=45, justify="left", wraplength=300, padx=10, pady=10)
+        self.text_instructions_label.grid(row=1, padx=10)
+
+        self.all_calcs_label = Label(self.history_frame, text="CAL goes here", padx=10, pady=10, bg=self.background, width=40, justify="left")
+        self.all_calcs_label.grid(row=2)
+
+        save_text = "Either choose a custom file name (and push \n <Export>) or simply push <Export> to save your \n CAL to a text file. if the \n file already exists, it will be overwritten."
+        self.save_instructions_label = Label(self.history_frame, text=save_text, wraplength=300, justify="left", width=40, padx=10, pady=10)
+        self.save_instructions_label.grid(row=3)
+
+        self.filename_entry = Entry(self.history_frame, font=("Arial", "12"), bg="#ffffff", width=25)
+        self.filename_entry.grid(row=4)
+
+        self.filename_error_label = Label(self.history_frame, text="Filename error goes here", fg="#9C0000", font=button_font_12)
+        self.filename_error_label.grid(row=5)
+
+        self.button_frame = Frame(self.history_frame)
+        self.button_frame.grid(row=6)
+
+        self.export_button = Button(self.button_frame, font=button_font_12, text="Export", bg="#004C99", fg="#FFFFFF", width=12, command=self.export_history)
+        self.export_button.grid(row=0, column=0, padx=10, pady=10)
+
+        self.dismiss_button = Button(self.button_frame, font=button_font_12, text="Dismiss", bg="#666666", fg="#FFFFFF", width=12, command=partial(self.close_history, partner))
+        self.dismiss_button.grid(row=0, column=1, padx=10, pady=10)
+
+    def close_history(self, partner):
+        partner.to_history_button.config(state=NORMAL)
+        self.history_box.destroy()
+
+    def export_history(self):
+        filename = self.filename_entry.get()
+        # Add functionality here to save the history to a file with the specified filename
+        # Handle filename validation and error messages
+        print(f"Exporting history to file: {filename}")
+
 if __name__ == "__main__":
-  root = Tk()
-  root.title("Temperature Converter")
-  Converter()
-  root.mainloop()
+    root = Tk()
+    root.title("Temperature Converter")
+    Converter()
+    root.mainloop()
