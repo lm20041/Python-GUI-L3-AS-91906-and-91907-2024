34e8fbc06b7626d9108f1ea3438d866beb221aab
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-03-23 23:14:45 +0000

    14 History GUI part 2 (V2)
    by removing this ".reverse()" in my V2 i have finished the component

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ while get_item != "xxx":
   get_item = input("Enter an item: ")
   if get_item != "xxx":
     all_calculations.append(get_item)
-  
-#test Line
-all_calculations.reverse()
+
 # Show all items in the list
 print("All items in the list:")
 for item in all_calculations:
