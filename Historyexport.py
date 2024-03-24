0b3af4e4653e0a18e34db01ca20b3663d42fec8f
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-03-24 02:55:39 +0000

    16 check filename

--- Historyexport.py
+++ Historyexport.py
@@ -2,75 +2,115 @@ from tkinter import *
 from functools import partial
 
 class Converter:
-    def __init__(self):
-        button_font_12 = ("Arial", 12, "bold")
-        button_fg = "#FFFFFF"
+  def __init__(self):
+    button_font_12 = ("Arial", 12, "bold")
+    button_fg = "#FFFFFF"
+    #5 item list
+    #self.all_calculations = ["0 F is -18 C", "0 C is 32 F", "30 F is -1", "30 C is 86 F", "40 F is 4 C"]
+    #6 item list
+    self.all_calculations = ["0 F is -18 C", "0 C is 32 F", "30 F is -1", "30 C is 86 F", "40 F is 4 C", "100 C is 212 F"]
+    # set up GUI frame
+    self.temp_frame = Frame(padx=10, pady=10)
+    self.temp_frame.grid()
 
-        self.temp_frame = Frame(padx=10, pady=10)
-        self.temp_frame.grid()
+    self.button_frame = Frame(padx=30, pady=30)
+    self.button_frame.grid(row=0)
 
-        self.button_frame = Frame(padx=30, pady=30)
-        self.button_frame.grid(row=0)
+    self.to_history_button = Button(self.button_frame, text="History / Export", bg="#004C99", fg=button_fg, font=button_font_12, width=12, state=DISABLED, command=lambda: self.to_history(self.all_calculations))
+    self.to_history_button.grid(row=1, column=1, padx=5, pady=5)
+    # ***** Remove when integrating *****
+    self.to_history_button.config(state=NORMAL)
 
-        self.to_history_button = Button(self.button_frame, text="History / Export", bg="#004C99", fg=button_fg, font=button_font_12, width=12, state=NORMAL, command=self.to_history)
-        self.to_history_button.grid(row=1, column=1, padx=5, pady=5)
-
-    def to_history(self):
-        DisplayHistory(self)
+  def to_history(self, all_calculations):
+    DisplayHistory(self, all_calculations)
 
 class DisplayHistory:
-    def __init__(self, partner):
-        self.background = "#ffe6cc"
-        button_font_12 = ("Arial", "12", "bold")
-        button_font_14 = ("Arial", "14", "bold")
-        self.history_box = Toplevel()
-        partner.to_history_button.config(state=DISABLED)
-        self.history_box.protocol('WM_DELETE_WINDOW', partial(self.close_history, partner))
+  def __init__(self, partner, calc_list):
+    #max number of calculations to be displayed
+    max_calcs = 5
+    self.var_max_calcs = IntVar()
+    self.var_max_calcs.set(max_calcs)
+    #convert cal list to string
+    calc_string_text = self.get_cals_string(calc_list)
 
-        self.history_frame = Frame(self.history_box, width=300, height=200)
-        self.history_frame.grid()
+    self.background = "#ffe6cc"
+    button_font_12 = ("Arial", "12", "bold")
+    button_font_14 = ("Arial", "14", "bold")
+    self.history_box = Toplevel()
 
-        self.history_heading_label = Label(self.history_frame, text="History / Export", font=button_font_14)
-        self.history_heading_label.grid(row=0)
+    partner.to_history_button.config(state=DISABLED)
+    self.history_box.protocol('WM_DELETE_WINDOW', partial(self.close_history, partner))
 
-        history_text = "Below are your recent CAL - \n showing 3 / 3 CAL. \n ALL CALS are shown to the nearest degree. \n"
-        self.text_instructions_label = Label(self.history_frame, text=history_text, width=45, justify="left", wraplength=300, padx=10, pady=10)
-        self.text_instructions_label.grid(row=1, padx=10)
+    self.history_frame = Frame(self.history_box, width=300, height=200)
+    self.history_frame.grid()
 
-        self.all_calcs_label = Label(self.history_frame, text="CAL goes here", padx=10, pady=10, bg=self.background, width=40, justify="left")
-        self.all_calcs_label.grid(row=2)
+    self.history_heading_label = Label(self.history_frame, text="History / Export", font=button_font_14)
+    self.history_heading_label.grid(row=0)
 
-        save_text = "Either choose a custom file name (and push \n <Export>) or simply push <Export> to save your \n CAL to a text file. if the \n file already exists, it will be overwritten."
-        self.save_instructions_label = Label(self.history_frame, text=save_text, wraplength=300, justify="left", width=40, padx=10, pady=10)
-        self.save_instructions_label.grid(row=3)
+    # decide which history of calculations to displays
+    num_calcs = len(calc_list)
+    if num_calcs > max_calcs:
+      calc_background = "#FFE6CC" #peach
+      showing_all = "here are your recent CALcs \n ({} / {} CAL shown). please export your Cal to see your full cal History".format(max_calcs, num_calcs)
+    else:
+      calc_background = "#B4FACB" #light green
+      showing_all = "here are your recent CALcs"
 
-        self.filename_entry = Entry(self.history_frame, font=("Arial", "12"), bg="#ffffff", width=25)
-        self.filename_entry.grid(row=4)
+    # History text and label
+    history_text = "{} \n\n ALL CALS are shown to the nearest degree.".format(showing_all)
+    self.text_instructions_label = Label(self.history_frame, text=history_text, width=40, justify="left", wraplength=300, padx=10, pady=10)
+    self.text_instructions_label.grid(row=1)
 
-        self.filename_error_label = Label(self.history_frame, text="Filename error goes here", fg="#9C0000", font=button_font_12)
-        self.filename_error_label.grid(row=5)
+    self.all_calcs_label = Label(self.history_frame, text=calc_string_text, padx=10, pady=10, bg=calc_background, width=40, justify="left")
+    self.all_calcs_label.grid(row=2)
 
-        self.button_frame = Frame(self.history_frame)
-        self.button_frame.grid(row=6)
+    save_text = "Either choose a custom file name (and push \n <Export>) or simply push <Export> to save your \n CAL to a text file. if the \n file already exists, it will be overwritten."
+    self.save_instructions_label = Label(self.history_frame, text=save_text, wraplength=300, justify="left", width=40, padx=10, pady=10)
+    self.save_instructions_label.grid(row=3)
 
-        self.export_button = Button(self.button_frame, font=button_font_12, text="Export", bg="#004C99", fg="#FFFFFF", width=12, command=self.export_history)
-        self.export_button.grid(row=0, column=0, padx=10, pady=10)
+    self.filename_entry = Entry(self.history_frame, font=("Arial", "12"), bg="#ffffff", width=25)
+    self.filename_entry.grid(row=4)
 
-        self.dismiss_button = Button(self.button_frame, font=button_font_12, text="Dismiss", bg="#666666", fg="#FFFFFF", width=12, command=partial(self.close_history, partner))
-        self.dismiss_button.grid(row=0, column=1, padx=10, pady=10)
+    self.filename_error_label = Label(self.history_frame, text="Filename error goes here", fg="#9C0000", font=button_font_12)
+    self.filename_error_label.grid(row=5)
 
-    def close_history(self, partner):
-        partner.to_history_button.config(state=NORMAL)
-        self.history_box.destroy()
+    self.button_frame = Frame(self.history_frame)
+    self.button_frame.grid(row=6)
 
-    def export_history(self):
-        filename = self.filename_entry.get()
-        # Add functionality here to save the history to a file with the specified filename
-        # Handle filename validation and error messages
-        print(f"Exporting history to file: {filename}")
+    self.export_button = Button(self.button_frame, font=button_font_12, text="Export", bg="#004C99", fg="#FFFFFF", width=12, command=self.export_history)
+    self.export_button.grid(row=0, column=0, padx=10, pady=10)
+
+    self.dismiss_button = Button(self.button_frame, font=button_font_12, text="Dismiss", bg="#666666", fg="#FFFFFF", width=12, command=partial(self.close_history, partner))
+    self.dismiss_button.grid(row=0, column=1, padx=10, pady=10)
+
+  def get_cals_string(self, var_cal):
+    #max_calcs
+    max_calcs = self.var_max_calcs.get()
+    calc_string = ""
+    #work out how many calculations to display
+    if len(var_cal) >= max_calcs:
+      stop = max_calcs
+    else:
+      stop = len(var_cal)
+    #list all but the last cal
+    for item in range(0, stop - 1):
+      calc_string += var_cal[len(var_cal) - item -1] + "\n"
+    #add last cal
+    calc_string += var_cal[-max_calcs]
+    return calc_string
+
+  def close_history(self, partner):
+    partner.to_history_button.config(state=NORMAL)
+    self.history_box.destroy()
+
+  def export_history(self):
+    filename = self.filename_entry.get()
+    # Add functionality here to save the history to a file with the specified filename
+    # Handle filename validation and error messages
+    print(f"Exporting history to file: {filename}")
 
 if __name__ == "__main__":
-    root = Tk()
-    root.title("Temperature Converter")
-    Converter()
-    root.mainloop()
+  root = Tk()
+  root.title("Temperature Converter")
+  Converter()
+  root.mainloop()
--- main.py
+++ main.py
@@ -1,116 +1,50 @@
-from tkinter import *
-from functools import partial
+from datetime import date
+import re
 
-class Converter:
-  def __init__(self):
-    button_font_12 = ("Arial", 12, "bold")
-    button_fg = "#FFFFFF"
-    #5 item list
-    #self.all_calculations = ["0 F is -18 C", "0 C is 32 F", "30 F is -1", "30 C is 86 F", "40 F is 4 C"]
-    #6 item list
-    self.all_calculations = ["0 F is -18 C", "0 C is 32 F", "30 F is -1", "30 C is 86 F", "40 F is 4 C", "100 C is 212 F"]
-    # set up GUI frame
-    self.temp_frame = Frame(padx=10, pady=10)
-    self.temp_frame.grid()
+# retreive date
+def get_date():
+  today = date.today()
+  day = today.strftime("%d")
+  month = today.strftime("%m")
+  year = today.strftime("%Y")
+  return "{}_{}_{}".format(day, month, year)
 
-    self.button_frame = Frame(padx=30, pady=30)
-    self.button_frame.grid(row=0)
-
-    self.to_history_button = Button(self.button_frame, text="History / Export", bg="#004C99", fg=button_fg, font=button_font_12, width=12, state=DISABLED, command=lambda: self.to_history(self.all_calculations))
-    self.to_history_button.grid(row=1, column=1, padx=5, pady=5)
-    # ***** Remove when integrating *****
-    self.to_history_button.config(state=NORMAL)
-
-  def to_history(self, all_calculations):
-    DisplayHistory(self, all_calculations)
-
-class DisplayHistory:
-  def __init__(self, partner, calc_list):
-    #max number of calculations to be displayed
-    max_calcs = 5
-    self.var_max_calcs = IntVar()
-    self.var_max_calcs.set(max_calcs)
-    #convert cal list to string
-    calc_string_text = self.get_cals_string(calc_list)
-    
-    self.background = "#ffe6cc"
-    button_font_12 = ("Arial", "12", "bold")
-    button_font_14 = ("Arial", "14", "bold")
-    self.history_box = Toplevel()
-    
-    partner.to_history_button.config(state=DISABLED)
-    self.history_box.protocol('WM_DELETE_WINDOW', partial(self.close_history, partner))
-
-    self.history_frame = Frame(self.history_box, width=300, height=200)
-    self.history_frame.grid()
-
-    self.history_heading_label = Label(self.history_frame, text="History / Export", font=button_font_14)
-    self.history_heading_label.grid(row=0)
-    
-    # decide which history of calculations to displays
-    num_calcs = len(calc_list)
-    if num_calcs > max_calcs:
-      calc_background = "#FFE6CC" #peach
-      showing_all = "here are your recent CALcs \n ({} / {} CAL shown). please export your Cal to see your full cal History".format(max_calcs, num_calcs)
+def check_filename(filename):
+  Problem = ""
+  # 
+  valid_char = "[A-Za-z0-9_]"
+  #
+  for letter in filename:
+    if re.match(valid_char, letter):
+      continue
+    elif letter == " ":
+      Problem = "sorry, no spaces allowed"
     else:
-      calc_background = "#B4FACB" #light green
-      showing_all = "here are your recent CALcs"
-    
-    # History text and label
-    history_text = "{} \n\n ALL CALS are shown to the nearest degree.".format(showing_all)
-    self.text_instructions_label = Label(self.history_frame, text=history_text, width=40, justify="left", wraplength=300, padx=10, pady=10)
-    self.text_instructions_label.grid(row=1)
+      Problem = ("sorry, no {}'s allowed".format(letter))
+    break
+  if Problem != "":
+    Problem = "{}. Use letters / numbers / underscores only".format(Problem)
+  return Problem
 
-    self.all_calcs_label = Label(self.history_frame, text=calc_string_text, padx=10, pady=10, bg=calc_background, width=40, justify="left")
-    self.all_calcs_label.grid(row=2)
+def filename_maker(filename):
+  #
+  if filename == "":
+    filename_ok = ""
+    date_part = get_date()
+    filename = "{}_Temperature_CAL".format(date_part)
+  else:
+    filename_ok = check_filename(filename)
 
-    save_text = "Either choose a custom file name (and push \n <Export>) or simply push <Export> to save your \n CAL to a text file. if the \n file already exists, it will be overwritten."
-    self.save_instructions_label = Label(self.history_frame, text=save_text, wraplength=300, justify="left", width=40, padx=10, pady=10)
-    self.save_instructions_label.grid(row=3)
+  if filename_ok == "":
+    filename += ".txt"
+  else:
+    filename = filename_ok
+  return filename
 
-    self.filename_entry = Entry(self.history_frame, font=("Arial", "12"), bg="#ffffff", width=25)
-    self.filename_entry.grid(row=4)
+# ***** Main Routine *****
+test_filenames = ["", "Test.txt", "Test it", "Test"]
 
-    self.filename_error_label = Label(self.history_frame, text="Filename error goes here", fg="#9C0000", font=button_font_12)
-    self.filename_error_label.grid(row=5)
-
-    self.button_frame = Frame(self.history_frame)
-    self.button_frame.grid(row=6)
-
-    self.export_button = Button(self.button_frame, font=button_font_12, text="Export", bg="#004C99", fg="#FFFFFF", width=12, command=self.export_history)
-    self.export_button.grid(row=0, column=0, padx=10, pady=10)
-
-    self.dismiss_button = Button(self.button_frame, font=button_font_12, text="Dismiss", bg="#666666", fg="#FFFFFF", width=12, command=partial(self.close_history, partner))
-    self.dismiss_button.grid(row=0, column=1, padx=10, pady=10)
-  
-  def get_cals_string(self, var_cal):
-    #max_calcs
-    max_calcs = self.var_max_calcs.get()
-    calc_string = ""
-    #work out how many calculations to display
-    if len(var_cal) >= max_calcs:
-      stop = max_calcs
-    else:
-      stop = len(var_cal)
-    #list all but the last cal
-    for item in range(0, stop - 1):
-      calc_string += var_cal[len(var_cal) - item -1] + "\n"
-    #add last cal
-    calc_string += var_cal[-max_calcs]
-    return calc_string
-    
-  def close_history(self, partner):
-    partner.to_history_button.config(state=NORMAL)
-    self.history_box.destroy()
-
-  def export_history(self):
-    filename = self.filename_entry.get()
-    # Add functionality here to save the history to a file with the specified filename
-    # Handle filename validation and error messages
-    print(f"Exporting history to file: {filename}")
-
-if __name__ == "__main__":
-  root = Tk()
-  root.title("Temperature Converter")
-  Converter()
-  root.mainloop()
+for item in test_filenames:
+  checked = filename_maker(item)
+  print("\n -<", checked)
+print("***** end progam *****")
